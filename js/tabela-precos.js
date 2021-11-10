0cbb88964d10acd959761b6a5a8b8b6d5a3b6599
Lucio <luciocarvalhodev@gmail.com>
2021-11-10 18:16:27 -0300

    start

---
+++ js/tabela-precos.js
@@ -0,0 +1,33 @@
+const listaServicos = [
+    {
+        grupo: 'Alicates',
+        servicos: [
+            ['Afiação', 7],
+            ['Troca de Mola', 2],
+            ['Alicate Mundial 522', 15],
+            ['Alicate Mundial 777', 22],
+            ['Alicate Mundial 722', 30]
+        ]
+    },
+    {
+        grupo: 'Chaves e Fechaduras',
+        servicos: [
+            ['Copia Chave Comum', 5],
+            ['Copia Chave Tetra', 20],
+            ['Copia Chave Carro/Moto', 20],
+            ['Copia Chave Armario', 10],
+            ['Chave Apartir de Fechadura', 10]
+        ]
+    },
+    {
+        grupo: 'Tesouras e Laminas',
+        servicos: [
+            ['Afiação Tesoura Unha', 4],
+            ['Afiação Tesoura', 5],
+            ['Afiação Faca', 3],
+            ['Afiação Facão', 4]
+        ]
+    }
+];
+
+let 
